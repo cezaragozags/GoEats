492bf7a7132a4acad5124df51552d14207e5af51
Cesar Zaragoza <zaragoza0711@gmail.com>
2025-02-23 22:23:52 -0600

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AsideBarComponent } from './aside-bar/aside-bar.component';
-import { CatalogoMenusComponent } from './catalogo-menus/catalogo-menus.component';
 import { FooterComponent } from './footer/footer.component';
-import { FormPlatillosComponent } from './form-platillos/form-platillos.component';
 import { HeaderComponent } from './header/header.component';
-import { ListPlatillosComponent } from './list-platillos/list-platillos.component';
 import { MainContainerComponent } from './main-container/main-container.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsideBarComponent, CatalogoMenusComponent, FooterComponent, FormPlatillosComponent, HeaderComponent, ListPlatillosComponent, MainContainerComponent],
+  imports: [AsideBarComponent, FooterComponent, HeaderComponent, MainContainerComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/main-container/main-container.component.ts
+++ src/app/main-container/main-container.component.ts
@@ -1,11 +1,90 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-container',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.css'
 })
+
 export class MainContainerComponent {
+  dropdownOpen = false;
+  selectedCategory = 'Desayuno';
+  categories = ['Desayuno', 'Comida', 'Cena'];
+  selectedImage: any=null;
+
+  newDish = {
+    title: '',
+    price: 0,
+    description: '',
+    image: '',
+    quantity: 0
+  };
+
+  dishes = [
+    { title: 'Huevos Estrellados', description: 'Elegir cantidad.', quantity: 2, price: 120, image: '/8.png', category: 'Desayuno' },
+    { title: 'Filete con Champiñones', description: 'Elegir termino de carne y complementos.', quantity: 0, price: 120, image: '/11.png', category: 'Comida' },
+    { title: 'Sincronizada', description: 'Elegir complemento.', quantity: 2, price: 120, image: '/14.png', category: 'Cena' }
+  ];
+
+  get filteredDishes() {
+    return this.dishes.filter(dish => dish.category === this.selectedCategory);
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+    this.dropdownOpen = false;
+  }
+
+
+  addDish() {
+    if (this.newDish.title && this.newDish.price > 0) {
+      let dish={
+        title:this.newDish.title,
+        price:this.newDish.price,
+        quantity:this.newDish.quantity,
+        description:this.newDish.description,
+        image:this.newDish.image,
+        category:this.selectedCategory
+      }
+      this.dishes.push(dish);
+      this.newDish = { title: '', price: 0, description: '', image: '', quantity: 0 };
+      this.selectedImage = null;
+    }
+  }
+
+    // Maneja la imagen seleccionada
+    onImageSelected(event: Event) {
+      const file = (event.target as HTMLInputElement).files?.[0];
+  
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.selectedImage = reader.result;
+          this.newDish.image = reader.result as string; // guarda la URL en el objeto del platillo
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  
+    // Limpia el formulario
+    clearForm() {
+      this.newDish = {
+        image: '',
+        title: '',
+        description: '',
+        quantity: 0,
+        price: 0
+      };
+      this.selectedImage = null;
+    }
+
+
 
 }
